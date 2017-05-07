1cf483da9e2860ee9e663210b31f4dccca441eef
omarzion <odama666@gmail.com>
2017-05-07 16:09:51 -0500

    Update mdl-modal-dialog.js

--- mdl-modal-dialog.js
+++ mdl-modal-dialog.js
@@ -2,7 +2,7 @@ function showLoading() {
   document.querySelector('.loading-container').remove();
   var container = document.createElement('div');
   container.setAttribute('id','orrsLoader');
-  container.innerHTML = `<div><div class="mdl-spinner mdl-js-spinner is-active"></div></div>`);
+  container.innerHTML = `<div><div class="mdl-spinner mdl-js-spinner is-active"></div></div>`;
   componentHandler.upgrade(container);
   document.querySelector('body').appendChild(container);
   container.style.opacity = 1;
